ef75a435d67fa6382b6c8826ce014e36a368d276
Arthur Vieira Silva <arthursilvavieira03@gmail.com>
2026-08-23 09:50:20 -0300

    Implementei a leitura do arquivo csv de entrada

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import sys
 import csv
 
-def read_input_file():
-    pass
+def read_input_file(file_path):
+    """Função utilizada para ler o arquivo csv de entrada e retornar as arestas obtidas."""
+
+    edges = []
+    try:
+        with open(file_path, 'r', newline='') as f:
+            print(f"Lendo o arquivo de entrada {file_path}...")
+            data = csv.reader(f)
+
+            for row in data:
+                edges.append(row) # cria uma lista com os valores da linha e adiciona na lista de arestas
+
+    except FileNotFoundError as error:
+        print(f"O arquivo {file_path} não existe!: {error}")
+        sys.exit(1)
+
+    return edges
 
 
 def run():
     # Verifica se os argumentos foram passados corretamente:
     if len(sys.argv) < 2:
         print("Argumentos insuficientes!")
-        print("Formato correto: python3 main.py <caminho_para_o_arquivo_de_entrada.csv>")
+        print("Formato correto: python main.py <caminho_para_o_arquivo_de_entrada.csv>")
         sys.exit(1)
+
+    file_path = sys.argv[1]
+    edges = read_input_file(file_path)
+    print(edges)
 
 if __name__ == "__main__":
     run()
